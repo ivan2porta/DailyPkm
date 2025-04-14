21a763f310d59fa14661a07331304d4489e254c1
ivan2porta <132475555+ivan2porta@users.noreply.github.com>
2025-04-14 23:30:17 +0200

    No funciona previsualización

--- src/app/components/buscador/buscador.component.ts
+++ src/app/components/buscador/buscador.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { crearPK } from '../../utils/helpers';
 
 @Component({
   selector: 'buscador',
   templateUrl: './buscador.component.html',
   styleUrls: ['./buscador.component.css']
 })
-export class BuscadorComponent {
+export class BuscadorComponent implements OnInit {
 
+  busqueda: string = '';
+  todosLosPokemonesId: number[] = [];
+  todosLosPokemonesNombre: string[] = [];
+  todosLosPokemonesImg: string[] = [];
+  sugerenciasId: number[] = [];
+  sugerenciasNombre: string[] = [];
+  sugerenciasImg: string[] = [];
+  sugerenciasContainer = document.getElementsByClassName("sugerencias-container")[0] as HTMLElement;
+
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<any>('https://pokeapi.co/api/v2/pokemon?limit=1025')
+      .subscribe(data => {
+        this.todosLosPokemonesId = data.results.map((p: any) => p.id);
+        this.todosLosPokemonesNombre = data.results.map((p: any) => p.name);
+        this.todosLosPokemonesImg = data.results.map((p: any) => p.sprites.front_default);
+      });
+  }
+
+  ngOnChange(): void{
+    this.filtrarPokemones();
+    for (let i = 0; i < this.todosLosPokemonesId.length; i++){
+      this.sugerenciasContainer.innerHTML += 
+      '<li><sugerencia [id]='+ this.todosLosPokemonesId[i] 
+      +' [nombre]='+ this.todosLosPokemonesNombre[i] 
+      + ' [img]='+ this.todosLosPokemonesImg[i] 
+      + '></sugerencia></li>' 
+    }
+  }
+
+  filtrarPokemones(): void {
+    if (this.busqueda.length >= 2) {
+      const valor = this.busqueda.toLowerCase();
+      this.sugerenciasNombre = this.todosLosPokemonesNombre.filter(nombre => nombre.includes(valor));
+      this.sugerenciasId = this.todosLosPokemonesId.filter(id => id.toString().includes(valor));
+      this.sugerenciasImg = this.todosLosPokemonesImg.filter(img => img.includes(valor));
+    } else {
+      this.sugerenciasNombre = [];
+      this.sugerenciasId = [];
+      this.sugerenciasImg = [];
+    }
+  }
+  
+
+  seleccionar(pokemon: string): void {
+    this.busqueda = pokemon;
+    this.sugerenciasNombre = [];
+    crearPK(this.busqueda);
+  }
 }
--- src/app/components/sugerencia/sugerencia.component.ts
+++ src/app/components/sugerencia/sugerencia.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'sugerencia',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./sugerencia.component.css']
 })
 export class SugerenciaComponent {
-
+  @Input() id: number = 0;
+  @Input() nombre: string = "";
+  @Input() img: string = "";
 }
